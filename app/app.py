b228d136794a31d52d083379b06ebd8f4e0e97eb
PIYAROT KHANTICHAT <piyarot.ongkillz@gmail.com>
2023-10-11 18:43:23 +0700

    init

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({'message': 'Hello, World!'})
+
+@app.route('/is_prime/<int:number>', methods=['GET'])
+def is_prime(number):
+    if number > 1:
+        for i in range(2, number):
+            if (number % i) == 0:
+                return 'False'
+            else:
+                return 'True'
+    else:
+        return 'False'
+    
+    
+if __name__=='__main__':
+    app.run(debug=True)
